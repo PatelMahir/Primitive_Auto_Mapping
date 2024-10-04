53fdeec4bbaebd7dce5ad8422431061db657ff48
PatelMahir <87660306+PatelMahir@users.noreply.github.com>
2024-10-04 16:39:32 +0530

    Combined all the different class Auto-Mappers together in Program.cs

--- Primitive_Auto_Mapping/Program.cs
+++ Primitive_Auto_Mapping/Program.cs
@@ -6,7 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
+//builder.Services.AddAutoMapper(typeof(Program).Assembly);
+//builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
